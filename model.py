67618441fd546c3ef375d41aa4e51f79111abfe6
csiki <chickenhun@gmail.com>
2022-02-23 04:33:58 +0100

    ResidualBlock implemented

--- model.py
+++ model.py
@@ -1,18 +1,20 @@
 import torch
+from torch.nn import Module, Sequential, ModuleList, \
+    Conv2d, BatchNorm2d, ReLU, Dropout2d, MaxPool2d, UpsamplingNearest2d, Identity
 
 
-class EncoderBlock(torch.nn.Module):
+class EncoderBlock(Module):
 
     def __init__(self, in_chans, out_chans, dropout, pool_size):
         super().__init__()
         kernel_size = (3, 2)
         stride = (1, 1)
 
-        self.conv = torch.nn.Conv2d(in_chans, out_chans, kernel_size, stride, bias=True)
-        self.bachnorm = torch.nn.BatchNorm2d(out_chans)
-        self.relu = torch.nn.ReLU()
-        self.dropout = torch.nn.Dropout2d(dropout)
-        self.maxpool = torch.nn.MaxPool2d(pool_size, pool_size)
+        self.conv = Conv2d(in_chans, out_chans, kernel_size, stride, bias=True, padding='same')
+        self.bachnorm = BatchNorm2d(out_chans)
+        self.relu = ReLU()
+        self.dropout = Dropout2d(dropout)
+        self.maxpool = MaxPool2d(pool_size, pool_size)
 
     def forward(self, x):
         x = self.conv(x)
@@ -23,18 +25,18 @@ class EncoderBlock(torch.nn.Module):
         return x
 
 
-class DecoderBlock(torch.nn.Module):
+class DecoderBlock(Module):
 
     def __init__(self, in_chans, out_chans, upsample_factor):
         super().__init__()
         kernel_size = (3, 2)
         stride = (1, 1)
 
-        self.conv = torch.nn.Conv2d(in_chans, out_chans, kernel_size, stride, bias=True)
-        self.bachnorm = torch.nn.BatchNorm2d(out_chans)
-        self.relu = torch.nn.ReLU()
+        self.conv = Conv2d(in_chans, out_chans, kernel_size, stride, bias=True, padding='same')
+        self.bachnorm = BatchNorm2d(out_chans)
+        self.relu = ReLU()
         # no dropout for decoder
-        self.upsample = torch.nn.UpsamplingNearest2d(scale_factor=upsample_factor)  # TODO try bilinear
+        self.upsample = UpsamplingNearest2d(scale_factor=upsample_factor)  # TODO try bilinear
 
     def forward(self, x):
         x = self.conv(x)
@@ -44,7 +46,39 @@ class DecoderBlock(torch.nn.Module):
         return x
 
 
-class ResidualBlock(torch.nn.Module):
-    def __init__(self):
+class ResidualBlock(Module):
+
+    def __init__(self, in_chans, out_chans, kernel_size):
+        super().__init__()
+        stride = (1, 1)
+        self.nsubblocks = 3
+        
+        self.first_skip_layer = Identity() if in_chans == out_chans \
+            else Conv2d(in_chans, out_chans, kernel_size, stride, bias=True, padding='same')
+
+        # TODO self.skip_layers = ModuleList([self.first_skip_layer, torch.nn.Identity(), torch.nn.Identity()])
+        self.activations = ModuleList([ReLU() for _ in range(self.nsubblocks)])
+        self.layers = ModuleList()  # the rest
+
+        for i in range(self.nsubblocks):
+            conv = Conv2d(in_chans, out_chans, kernel_size, stride, bias=True, padding='same')
+            bachnorm = BatchNorm2d(out_chans)
+            self.layers.append(Sequential(conv, bachnorm))
+            in_chans = out_chans
+
+    def forward(self, x):
+        res = self.first_skip_layer(x)
+        for i in range(self.nsubblocks):
+            x = self.layers[i](x)
+            x = self.activations[i](x)
+        return x + res
+
+
+class EMGModel(Module):
+
+    def __init__(self, dropout, res_block_params):
         super().__init__()
         # TODO
+
+    def forward(self, x):
+        pass  # TODO
